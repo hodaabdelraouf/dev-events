e8040b7bf30b2f3dc38a7686e4c4ae1bda9d256e
hodaabdelraouf <hodaabdelraouf9@gmail.com>
2025-11-22 19:16:56 +0200

    Implement api routes, server actions, caching and event details page

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const martianMono = Martian_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "DevEvent",
+  title: "DevEvents",
   description: "The Hub for Every Dev Event You Mustn't Miss",
 };
 
